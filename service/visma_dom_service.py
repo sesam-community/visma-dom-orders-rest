8de1758bb9f1bc2467f8ccb43b83c9a42b72582f
Timur Samkharadze <timur.samkharadze@sysco.no>
2019-04-01 12:29:46 +0200

    adding cherrypy server better error messages since support

--- service/visma_dom_service.py
+++ service/visma_dom_service.py
@@ -3,16 +3,20 @@
 import os
 import logging
 import json
-from urllib.parse import urlencode
+from utils import str_to_bool
+from datetime import datetime
 
 import requests
+
 from flask import Flask, request, Response, abort
 
 APP = Flask(__name__)
 
-LOG_LEVEL = os.environ.get('LOG_LEVEL', logging.DEBUG)
+LOG_LEVEL = os.environ.get('LOG_LEVEL', "INFO")
 
 API_PATH = os.environ.get('API_BASE_PATH')
+
+CONVERT_TS_TO_DATE_STR = str_to_bool(os.environ.get('CONVERT_TS_TO_DATE_STR', 'false'))
 
 if not API_PATH:
     logging.error("API_PATH required")
@@ -20,6 +24,11 @@ if not API_PATH:
 
 # at least Ocp-Apim-Subscription-Key header with API access key must be provided
 HEADERS = json.loads(os.environ.get("HEADERS", '{}'))
+
+if not HEADERS.get('Ocp-Apim-Subscription-Key'):
+    logging.error("Subscription key not found")
+    exit(1)
+
 PORT = os.environ.get('PORT', 5001)
 
 
@@ -35,43 +44,60 @@ def process(headers, req_args):
     """processes API call
     """
     yield "["
-    offset = 0
-    items_to_take = 20
     first = True
-    while True:
-        since = req_args.get('since')
-        url = build_api_request_url(API_PATH, {'skip': offset, 'take': items_to_take}, since)
-        logging.debug("Send request to %s", url)
-        response = requests.get(url, headers=headers)
-        if response.status_code != 200:
-            abort(response.status_code, response.text)
-        result = response.json()
-        offset += items_to_take
 
-        for item in result:
-            if not first:
-                yield ","
-            else:
-                first = False
-            yield json.dumps(item)
+    since = req_args.get('since')
+    url = build_api_request_url(API_PATH, since)
+    logging.debug("Send request to %s", url)
+    response = requests.get(url, headers=headers)
 
-        if len(result) < items_to_take:
-            break
+    if response.status_code != 200:
+        abort(response.status_code, response.text)
+
+    result = response.json()
+
+    for item in result:
+        if not first:
+            yield ","
+        else:
+            first = False
+
+        item["_updated"] = datetime.fromtimestamp(
+            item["orderChangedDate"]).astimezone().isoformat() if CONVERT_TS_TO_DATE_STR else item[
+            "orderChangedDate"]
+        yield json.dumps(item)
     yield "]"
 
 
-def build_api_request_url(api_path, query_dict=None, since=None):
+def build_api_request_url(api_path, since=None):
     """builds URL for API request"""
     payload = ''
-    if query_dict:
-        payload = '?' + urlencode(query_dict)
 
-    if since is not None:
-        payload += '&fromDate=' + since
+    if since:
+        payload += '?fromDate=' + since
     return api_path + payload
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=LOG_LEVEL)
+    logging.basicConfig(level=logging.getLevelName(LOG_LEVEL))
+
     IS_DEBUG_ENABLED = True if logging.getLogger().isEnabledFor(logging.DEBUG) else False
-    APP.run(debug=IS_DEBUG_ENABLED, host='0.0.0.0', port=PORT)
+
+    if IS_DEBUG_ENABLED:
+        APP.run(debug=IS_DEBUG_ENABLED, host='0.0.0.0', port=PORT)
+    else:
+        import cherrypy
+
+        cherrypy.tree.graft(APP, '/')
+        cherrypy.config.update({
+            'environment': 'production',
+            'engine.autoreload_on': True,
+            'log.screen': True,
+            'server.socket_port': PORT,
+            'server.socket_host': '0.0.0.0',
+            'server.thread_pool': 10,
+            'server.max_request_body_size': 0
+        })
+
+        cherrypy.engine.start()
+        cherrypy.engine.block()
